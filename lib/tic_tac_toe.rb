2374178ed29518faa2f63c9958416a3ff4c0d0bb
Matteo Muscas <thatman.j@gmail.com>
2018-10-08 09:03:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,7 +97,7 @@ end
 
 def play(board)
       turn(board) until over?(board)
-    if won?
+    if won?(board)
       puts "Congratulations #{winner(board}!"
     elsif draw?(board)
       puts "Cat\'s Game!"
